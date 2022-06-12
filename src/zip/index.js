64b429e52e1f68c3572f90b67ed9408b8aee426d
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-12 23:39:39 +0300

    feat: add decompress solution

--- src/zip/index.js
+++ src/zip/index.js
@@ -1,11 +1,13 @@
 import compress from "./compress.js";
+import decompress from "./decompress.js";
 const zipController = async (command, args, homeDir) => {
   switch (command) {
     case "compress":
       await compress(args, homeDir);
       break;
-    // case 'decompress':
-    // await decompress()
+    case "decompress":
+      await decompress(args, homeDir);
+      break;
     default:
       break;
   }
