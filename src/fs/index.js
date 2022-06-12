8b164512cc4568771ff79eed53bb10046084bee9
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-12 23:33:08 +0300

    feat: solution for add, cat, rn commands

---
+++ src/fs/index.js
@@ -0,0 +1,35 @@
+import * as logs from "../console-colors/index.js";
+import cat from "./cat.js";
+import add from "./add.js";
+import rn from "./rn.js";
+// import cp from "./cp.js";
+// import mv from "./mv.js";
+// import rm from "./rm.js";
+
+const fsController = async (command, args, homeDir, rootDir) => {
+  switch (command) {
+    case "cat":
+      await cat(args, homeDir);
+      break;
+    case "add":
+      await add(args, homeDir);
+      break;
+    case "rn":
+      await rn(args, homeDir);
+      break;
+    // case "cp":
+    //   await cp();
+    //   break;
+    // case "mv":
+    //   await mv();
+    //   break;
+    // case "rm":
+    //   await rm();
+    //   break;
+    default:
+      logs.IILog();
+      break;
+  }
+};
+
+export default fsController;
